2fb95300e00d4641acb43d1646c42dd0cf42f233
Peter Pan <ALostBoyFromNeverland@outlook.com>
2018-03-07 23:45:33 -0500

    Initial app commit
    Created a basic server with express and added necessary dependencies needed in future development

---
+++ app.js
@@ -0,0 +1,27 @@
+// App dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// App definition
+const app = express();
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// body-parser Middleware
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', (req, res) => {
+    res.send("Invalid endpoint");
+});
+
+// Start app
+app.listen(port, () => {
+    console.log("Server started on PORT: " + port);
+});
